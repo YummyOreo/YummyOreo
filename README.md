41765f30b2e697d41e776ce796747dcf6dc5f946
YummyOreo <bobgim20@gmail.com>
2023-01-27 16:27:34 -0600

    adds stuff?

--- README.md
+++ README.md
@@ -5,16 +5,16 @@ impl YummyOreo {
             interests: vec!["Coding", "Ultimate Frisbee", "Games", "Immunology", "Dnd", "Panting minis"],
             code: vec![Lang::Rust, Lang::Python, Lang::Typescript],
             tools: vec![Tool::Neovim, Tool::Mongobd, Tool::Gimp, Tool::Figma],
-            
+
             site: "oreo.is-a.dev",
         }
     }
-    
+
     pub fn current_project(&self) -> Project {
         Project::Secret
     }
-    
+
     pub fn latest_blogs() -> Vec<Blog> {
-        // TODO: Implement this
     }
 }
+```
--- index.js
+++ index.js
@@ -4,39 +4,37 @@ const Parser = require("rss-parser");
 const parser = new Parser();
 
 const feedUrl = "https://oreo.is-a.dev/rss.xml";
+const blogPostLimit = 5;
 
-md.use(emoji);
-
-(async () => {
-  let blogPosts = "";
-  try {
-    blogPosts = await loadBlogPosts();
-  } catch (e) {
-    console.error(`Failed to load blog posts from ${websiteUrl}`, e);
-  }
-
-  fs.readFile('README.md', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  let md = data;
-    const reg = /\s+pub fn latest_blogs\(\) -> Vec<Blog> {(\n.*){0,}}/;
-    const newStr = str.replace(reg, `    pub fn latest_blogs() -> Vec<Blog> {\n{        {data}\n    }`);
-    fs.writeFile("README.md", newStr, function (err) {
-    if (err) return console.log(err);
-    console.log(`${newStr} > README.md`);
-  });
-});
-})();
+loadBlogPosts()
 
 async function loadBlogPosts() {
-  const feed = await parser.parseURL(feedUrl);
+    const feed = await parser.parseURL(feedUrl);
+    // console.log(feed)
 
-  let links = "";
+    let links = "";
 
-  feed.items.slice(0, blogPostLimit).forEach((item) => {
-    links += `        Blog {title: {item.title}, url: {item.link}},\n`;
-  });
-  return `vec![\n{links}\n]`
+    feed.items.slice(0, blogPostLimit).forEach((item) => {
+        links += `           Blog {title: \"${item.title}\", url: \"${item.link}\"},`;
+    });
+    // console.log(links)
+    //
+    let blogPosts = `vec![\n${links}\n        ]`;
+    console.log(blogPosts)
+
+    fs.readFile('README.md', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        let md = data;
+        const reg = /\s+pub fn latest_blogs\(\) -> Vec<Blog> {\n(.*\n){0,}\s+}/;
+        console.log(blogPosts)
+        const newStr = md.replace(reg, "\n\n    pub fn latest_blogs() -> Vec<Blog> {\n        " + blogPosts + "\n    }");
+        console.log(newStr)
+        fs.writeFile("README.md", newStr, function(err) {
+            if (err) return console.log(err);
+            console.log(`${newStr} > README.md`);
+        });
+    });
 }
